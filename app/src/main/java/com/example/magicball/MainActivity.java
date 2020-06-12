33d0044f0a1809f51b6a2fc31572bb2ffa40533d
EstherQ999 <esther.quispe@mail.udp.cl>
2020-06-12 15:04:12 -0400

    segundo commit proyecto android

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.magicball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView ball;
+    private TextView respuestaDestino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //referencia imagen
+        ball = findViewById(R.id.boton);
+
+        //referencia segundo texto
+        respuestaDestino = findViewById(R.id.respuesta);
+
+        ball.setOnClickListener((View.OnClickListener) this);
+        Toast.makeText(MainActivity.this, "Buscando tu destino!!", Toast.LENGTH_SHORT).show();
+
     }
+
+
 }
